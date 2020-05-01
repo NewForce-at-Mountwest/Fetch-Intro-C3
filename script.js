7a638b8d99848f50d2ea89fabe0782334aa6c725
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-05-01 09:59:14 -0400

    un-commented out example code

--- script.js
+++ script.js
@@ -14,29 +14,28 @@
 
 // city id : 1138
 
+// This fetch call won't actually work!
 fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=mexican", {
-    headers: {
-        "user-key": "ac8ce1c0bab66fafebad3e4d78ef9643",
-    }
 }) //go rob the bank
   .then((dirtyMoney) => dirtyMoney.json()) //cleaning the money
   .then(function (parsedFoods) {
+    // This is all of the foods
     console.log(parsedFoods);
     // Select the container where we want to print
-    // const foodContainer = document.querySelector(".foodList")
+    const foodContainer = document.querySelector(".foodList")
 
-    // // Loop through the foods when they come back from the API
-    // parsedFoods.forEach(singleFood => {
-    //     // For each food in the array, print it to the DOM
-    //     foodContainer.innerHTML += `
-    //     <div>
-    //         <h4>${singleFood.name}</h4>
-    //         <p>${singleFood.category}</p>
-    //         <p>${singleFood.ethnicity}</p>
-    //     </div>`
+    // Loop through the foods when they come back from the API
+    parsedFoods.forEach(singleFood => {
+        // For each food in the array, print it to the DOM
+        foodContainer.innerHTML += `
+        <div>
+            <h4>${singleFood.name}</h4>
+            <p>${singleFood.category}</p>
+            <p>${singleFood.ethnicity}</p>
+        </div>`
   });
 
-  fetch(`zomato.com&apiKey=${zomatoAPIKey}`)
-  .then(r => r.json())
-  .then(r => console.log(r))
+//   fetch(`zomato.com&apiKey=${zomatoAPIKey}`)
+//   .then(r => r.json())
+//   .then(r => console.log(r))
 
