dba66bc9ef71c0f234b7c5fee729c6c4bc8d7740
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-05-01 09:13:31 -0400

    added apikeyfile

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-             //https://api.openbrewerydb.org/breweries           http://localhost:8088/food
+//https://api.openbrewerydb.org/breweries           http://localhost:8088/food
 
 // fetch("https://api.openbrewerydb.org/breweries")
 // .then(breweries => breweries.json())
@@ -10,27 +10,33 @@
 
 // })
 
+// search?entity_id=37217&q=mexican
 
+// city id : 1138
 
-fetch("http://localhost:8088/food")                     //go rob the bank
-.then(dirtyMoney => dirtyMoney.json())                //cleaning the money
-.then(function(parsedFoods){
+fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=mexican", {
+    headers: {
+        "user-key": "ac8ce1c0bab66fafebad3e4d78ef9643",
+    }
+}) //go rob the bank
+  .then((dirtyMoney) => dirtyMoney.json()) //cleaning the money
+  .then(function (parsedFoods) {
+    console.log(parsedFoods);
     // Select the container where we want to print
-    const foodContainer = document.querySelector(".foodList")
+    // const foodContainer = document.querySelector(".foodList")
 
-    // Loop through the foods when they come back from the API
-    parsedFoods.forEach(singleFood => {
-        // For each food in the array, print it to the DOM
-        foodContainer.innerHTML += `
-        <div>
-            <h4>${singleFood.name}</h4>
-            <p>${singleFood.category}</p>
-            <p>${singleFood.ethnicity}</p>
-        </div>`
-    })
-})
+    // // Loop through the foods when they come back from the API
+    // parsedFoods.forEach(singleFood => {
+    //     // For each food in the array, print it to the DOM
+    //     foodContainer.innerHTML += `
+    //     <div>
+    //         <h4>${singleFood.name}</h4>
+    //         <p>${singleFood.category}</p>
+    //         <p>${singleFood.ethnicity}</p>
+    //     </div>`
+  });
 
-
-
-
+  fetch(`zomato.com&apiKey=${zomatoAPIKey}`)
+  .then(r => r.json())
+  .then(r => console.log(r))
 
